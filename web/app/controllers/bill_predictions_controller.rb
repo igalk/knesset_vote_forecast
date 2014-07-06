391c4f9e3289d9db413c1eb65b58240a881311f0
Igal Kreichman <igal@skycure.com>
2014-07-06 11:24:50 +0300

    Remove unnecasery comment

--- web/app/controllers/bill_predictions_controller.rb
+++ web/app/controllers/bill_predictions_controller.rb
@@ -2,7 +2,6 @@ class BillPredictionsController < ApplicationController
   respond_to :json
 
   def index
-    # Gather all post data
     predictions = BillPrediction.joins(:bill).select('bills.title, bills.state, bill_predictions.prediction')
 
     render json: predictions.as_json(except: 'id')
